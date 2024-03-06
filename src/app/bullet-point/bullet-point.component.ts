a93404e44c453fd65cb7ca255d242f7abb3eca09
Jason Kepler <jasonkepler1@gmail.com>
2024-03-06 11:00:11 -0800

    Generate key components

---
+++ src/app/bullet-point/bullet-point.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bullet-point',
+  standalone: true,
+  imports: [],
+  templateUrl: './bullet-point.component.html',
+  styleUrl: './bullet-point.component.css'
+})
+export class BulletPointComponent {
+
+}
