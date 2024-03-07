06e090044af401a52df377274d3c02cb539780e2
Jason Kepler <jasonkepler1@gmail.com>
2024-03-06 16:01:55 -0800

    Add filter by position
    Get radio buttons and basic filtering working.

--- src/app/experience-list/experience-list.component.ts
+++ src/app/experience-list/experience-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ExperienceComponent } from '../experience/experience.component';
 import { Experience } from '../Experience.interface';
 import { ExperienceService } from '../experience.service';
@@ -6,14 +7,31 @@ import { ExperienceService } from '../experience.service';
 @Component({
   selector: 'app-experience-list',
   standalone: true,
-  imports: [ExperienceComponent],
+  imports: [ExperienceComponent, ReactiveFormsModule],
   templateUrl: './experience-list.component.html',
   styleUrl: './experience-list.component.css'
 })
 export class ExperienceListComponent {
-  experiences: Experience[] = []
+  experiences: Experience[] = [];
+  filteredExperiences: Experience[] = [];
   experienceService: ExperienceService = inject(ExperienceService);
+  filterControl = new FormControl('all');
+
   constructor() {
     this.experiences = this.experienceService.getAllExperiences();
+    this.filteredExperiences = this.experiences;
+
+    this.filterControl.valueChanges.subscribe(() => {
+      this.filterExperiences();
+    });
+  }
+
+  filterExperiences(): void {
+    if (this.filterControl.value === 'all') {
+      this.filteredExperiences = this.experiences;
+    } else {
+      this.filteredExperiences = this.experiences.filter(
+        experience => experience.position === this.filterControl.value);
+    }
   }
 }
