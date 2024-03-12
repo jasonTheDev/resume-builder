09946b11033ebdc60a645b96c9cf406f04d05214
Jason Kepler <jasonkepler1@gmail.com>
2024-03-12 16:18:07 -0700

    Delete bulletPoints

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { BulletPointComponent } from '../bullet-point/bullet-point.component';
 import { Experience } from '../Experience.interface';
+import { BulletPoint } from '../BulletPoint.interface';
 
 @Component({
   selector: 'app-experience',
@@ -13,4 +14,7 @@ export class ExperienceComponent {
 
   @Input() experience!: Experience;
 
+  remove(bullet: BulletPoint) {
+    this.experience.bulletPoints = this.experience.bulletPoints.filter(b => b !== bullet);
+  }
 }
