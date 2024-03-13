700574afc23c3e62dac6d78213a364a57caeecca
Jason Kepler <jasonkepler1@gmail.com>
2024-03-13 15:18:03 -0700

    Delete experiences

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { BulletPointComponent } from '../bullet-point/bullet-point.component';
 import { Experience } from '../Experience.interface';
 import { BulletPoint } from '../BulletPoint.interface';
@@ -17,6 +17,7 @@ export class ExperienceComponent {
   bulletControl = new FormControl('');
 
   @Input() experience!: Experience;
+  @Output() removeExperience = new EventEmitter<Experience>();
 
   remove(bullet: BulletPoint) {
     this.experience.bulletPoints = this.experience.bulletPoints.filter(b => b !== bullet);
